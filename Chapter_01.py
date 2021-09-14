e67394526c235c18736d02793a98f3c638bb7875
Lemony <csf2412297817@163.com>
2021-09-14 12:29:54 +0800

    add

--- Chapter_01.py
+++ Chapter_01.py
@@ -1,11 +1,11 @@
 '''
-二分算法-refs:图解算法page7
+二分查找-refs:图解算法page7
 '''
 def binary_search(sortedArray, targetItem) -> int:
     low = 0
     high = len(sortedArray) - 1
     while low <= high:                          #只要查找范围在两个元素及以上
-        mid = (low + high) >> 2
+        mid = (low + high) >> 1
         guess = sortedArray[mid]
         if guess == targetItem:
             return mid
---
+++ Chapter_02.py
@@ -0,0 +1,21 @@
+def findmin(arrayToSort):
+    min = arrayToSort[0]
+    minIndex = 0
+    for i in range(len(arrayToSort)):
+        if arrayToSort[i] < min:
+            min = arrayToSort[i]
+            minIndex = i
+
+    return minIndex
+
+# 时间复杂度 o(n^2)
+def selectionSort(arrayToSort):
+    sortedArray = []
+    for i in range(len(arrayToSort)):
+        min = findmin(arrayToSort)
+        sortedArray.append(arrayToSort.pop(min))
+    return sortedArray
+
+
+print(selectionSort([5,3,6,2,10]))
+
