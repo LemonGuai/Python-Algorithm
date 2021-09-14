836b886c05bf3431953327ae4ec04b2d1028a58d
Lemony <csf2412297817@163.com>
2021-09-14 10:12:36 +0800

    Update Chapter_01.py

--- Chapter_01.py
+++ Chapter_01.py
@@ -1,16 +1,20 @@
+'''
+二分算法-refs:图解算法page7
+'''
 def binary_search(sortedArray, targetItem) -> int:
     low = 0
     high = len(sortedArray) - 1
-    while low <= high:
+    while low <= high:                          #只要查找范围在两个元素及以上
         mid = (low + high) >> 2
-        if sortedArray[mid] > targetItem:
-            high = mid - 1
-        elif sortedArray[mid] < targetItem:
-            low = mid + 1
-        else:
+        guess = sortedArray[mid]
+        if guess == targetItem:
             return mid
+        elif guess > targetItem:
+            high = mid - 1
+        else :
+            low = mid + 1
     return None
 
 
-testSortedArray = [1,3,4,5,6,7,8,9,10,11]
-print(binary_search(testSortedArray,11)==(len(testSortedArray)-1))
+testSortedArray = [1,3,5,7,9]
+print(binary_search(testSortedArray,-1)==None)
